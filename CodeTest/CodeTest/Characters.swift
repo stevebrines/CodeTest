a2f5019d41eb93731faf1ad4c5ebc084684ebf31
Brines, Steve <SteveBrines@quickenloans.com>
2021-02-04 14:09:53 -0500

    Initial commit

---
+++ CodeTest/CodeTest/Characters.swift
@@ -0,0 +1,34 @@
+//
+//  Characters.swift
+//  CodeTest
+//
+//  Created by Brines, Steve on 2/4/21.
+//
+
+import Foundation
+
+struct Characters {
+    struct Scorpion {
+        static let name = "Scorpion"
+        static let deck = "Scorpion is a wraith from the Netherrealm who is consumed with a desire for revenge."
+        static let aliases = "Hanzo Hasashi"
+        static let imageName = "Scorpion_Image"
+        static let thumgnailImage = "Scorpion_Thumbnail"
+    }
+
+    struct Zelda {
+        static let name = "Zelda"
+        static let deck = "Zelda is the titular figure of The Legend of Zelda. She is most often depicted as the princess of Hyrule."
+        static let aliases = "Princess Zelda\r\nSheik\r\nShiek\r\nTetra\r\nQueen Zelda\r\nHylia"
+        static let imageName = "Zelda_Image"
+        static let thumgnailImage = "Zelda_Thumbnail"
+    }
+
+    struct Yoda {
+        static let name = "Yoda"
+        static let deck = "Yoda is one of the most powerful Jedi Masters to have ever lived. He is easily recognized by his short stature, green skin, pointy ears, and speech patterns."
+        static let aliases = ""
+        static let imageName = "Yoda_Image"
+        static let thumgnailImage = "Yoda_Thumbnail"
+    }
+}
---
+++ CodeTest/CodeTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CodeTest
+//
+//  Created by Brines, Steve on 2/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
